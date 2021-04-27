b05b303abeb224b4e583257aa27f98e33ac7cb4a
Your Name <you@example.com>
2021-04-27 13:07:48 -0300

    Beta verion

---
+++ app/src/main/java/com/nicergames/a2048/MainActivity.java
@@ -0,0 +1,32 @@
+package com.nicergames.a2048;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                openJuego();
+            }
+        });
+    }
+
+    public void openJuego() {
+        Intent intent = new Intent(this, Juego.class);
+        startActivity(intent);
+    }
+}
+
